c400f1ec3133b2308c923d2e9e7e11b1be483afb
1manaliyev <baxtik.kkp@mail.ru>
2025-09-09 01:52:02 +0500

    Update apiPaths.js

--- frontend/src/apiPaths.js
+++ frontend/src/apiPaths.js
@@ -1,8 +1,8 @@
 const apiBase = import.meta.env.VITE_API
 
 export default {
-  channels: `${import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN}/channels`,
+  channels: `${apiBase}/channels`,
   messages: `${apiBase}/messages`,
   login: `${apiBase}/login`,
   signup: `${apiBase}/signup`,
-}
+};
