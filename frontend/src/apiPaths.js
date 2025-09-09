4e3e91b5eba0f686943682b6be16a1414a71cfdb
1manaliyev <baxtik.kkp@mail.ru>
2025-09-09 17:04:17 +0500

    Create apiPaths.js

---
+++ frontend/src/apiPaths.js
@@ -0,0 +1,8 @@
+const apiBase = import.meta.env.VITE_API;
+
+export default {
+  channels: `${import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN}/channels`,
+  messages: `${apiBase}/messages`,
+  login: `${apiBase}/login`,
+  signup: `${apiBase}/signup`,
+};
